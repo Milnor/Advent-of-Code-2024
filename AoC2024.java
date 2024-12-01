e37c1b84d3e5aba15f86b4e8abf278e14cc493b9
Milnor <Milnor@users.noreply.github.com>
2024-12-01 15:50:49 -0500

    day 1 part 1

---
+++ AoC2024.java
@@ -0,0 +1,43 @@
+//import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class AdventOfCode
+{
+    public static void main(String []args)
+    {
+        //System.out.println("Hello, AoC 2024!");
+        String filePath = "inputs/day01.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+
+            ArrayList<Integer> column1 = new ArrayList<Integer>();
+            ArrayList<Integer> column2 = new ArrayList<Integer>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                //System.out.println(line);
+                String[] inputs = line.split("\\s+");
+                column1.add(Integer.parseInt(inputs[0]));
+                column2.add(Integer.parseInt(inputs[1]));
+            }
+            Collections.sort(column1);
+            Collections.sort(column2);
+
+            int total = 0;
+            for (int i = 0; i < column1.size(); i++) {
+                total += Math.abs(column1.get(i) - column2.get(i));
+            }
+
+            System.out.println(total);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
